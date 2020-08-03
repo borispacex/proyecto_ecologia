a04a5486fc9796045c2cb8d455b5e3ed083be943
Boris <borisvargaspaucara@gmail.com>
2020-08-02 22:42:34 -0400

    haremos cambios al subir archivos, pruebas

--- front-end/src/app/services/global.ts
+++ front-end/src/app/services/global.ts
@@ -1,6 +1,6 @@
 
-const host = 'http://localhost:8012';
-// const host = 'https://umsa-ecologia.xyz/v1';
+// const host = 'http://localhost:8012';
+const host = 'https://umsa-ecologia.xyz/v1';
 
 export var GLOBAL = {
     url: `${host}/api/`, // http --> https
